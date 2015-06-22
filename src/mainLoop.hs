3eb4d6bbeefda36ff0ef38572b910ce6e507bf83
Mike Houghton <mike@Mikes-MacBook-Air.local>
2015-06-22 22:04:21 +0100

    in progress

--- src/mainLoop.hs
+++ src/mainLoop.hs
@@ -20,14 +20,29 @@ import System.IO
 
 import System.Directory
 -- ==============================================================================================================
-saveMoves::FilePath -> Moves -> IO()
+
+
+saveGame :: FilePath -> Game -> IO()
+saveGame path game = do
+  writeFile path (show game)
+
+loadGame :: FilePath -> IO(Game)
+loadGame path = do
+  withFile path ReadMode (\h -> do
+    -- lazy evalso force it (I think thats it!! with ! it won't work, or will if do print contents)
+    !contents <- hGetContents h
+
+    let game = read contents::Game
+    return $ game
+    )
+
+
+saveMoves :: FilePath -> Moves -> IO()
 saveMoves path moves = do
   writeFile path (show moves)
-
-
   
 
-loadMoves::FilePath -> IO(Moves)
+loadMoves :: FilePath -> IO(Moves)
 loadMoves path = do
   withFile path ReadMode (\h -> do
     -- lazy evalso force it (I think thats it!! with ! it won't work, or will if do print contents)
@@ -67,6 +82,7 @@ createGame game mail = do
     createDirectoryIfMissing True $ dir ++ "/" ++ id ++ "/" ++ "1"
     print $ dir ++ "/" ++ id ++ "/" ++ "1"
     -- write game details 
+
     writeFile (dir ++ "/" ++ id ++ "/game") (show game)
 
 
@@ -79,15 +95,15 @@ handleMoves moves mail = do
     Left msg -> handleError msg mail 
     Right subj -> do     
       userDir <- getAppUserDataDirectory "mapmoves"
-      let to =  mailFrom mail
+      let to = extractAddr $ mailFrom mail
           id = gameId subj
           
       --  sub = (gameId subj ++ " Turn " ++ show (turnNum subj  + 1))
-          sub = gameId subj ++ " Turn " ++ show (turnNum subj)
+          sub = id ++ " Turn " ++ show (turnNum subj)
           
            -- to could have format "Mike Houghton <mike_k_houghton@yahoo.co.uk>"
-          folderName =  userDir ++ "/" ++ id ++ "/" ++ show (turnNum subj) ++ "/" ++ (extractAddr to)
-          ms = " OK! Received. "
+          folderName =  userDir ++ "/" ++ id ++ "/" ++ show (turnNum subj) ++ "/" ++ to
+          ms = " OK! Moves received. Please be patient.  I'm still waiting for your opponents moves"
                
       saveMoves folderName moves
 
@@ -95,19 +111,21 @@ handleMoves moves mail = do
       let fInPath = getFilesInPath rootF
 
      
-
+      -- 2 files ?
       count' <- monadicLength $ fInPath
       case count' of
       	1 -> do
           -- just say ok -received your moves
       		postMail to sub ms
         2 -> do
+
         	fPath <- fInPath 
         	let (m1, m2) = (loadMoves $  (fPath !! 0), loadMoves $ (fPath !! 1))
         	print fPath
         	m1' <- m1
         	m2' <- m2
-        	
+        	g   <- loadGame (userDir  ++ "/" ++ id ++ "/game")
+          
           -- any common points?
         	let pathsX = pathsCross  (getAllTo m1' ) (getAllTo m2' )
         	print "/n"
